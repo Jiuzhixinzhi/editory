bfb929a90dd50f7ff3d39bb1f7853df6428c5cf4
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2024-08-19 18:22:20 +0800

    Remove GitHub provider

--- app/login.tsx
+++ app/login.tsx
@@ -2,7 +2,7 @@
 
 import { signIn } from 'next-auth/react'
 import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@nextui-org/react'
-import { PiSignInDuotone, PiGithubLogoDuotone, PiGoogleLogoDuotone } from 'react-icons/pi'
+import { PiSignInDuotone, PiGoogleLogoDuotone } from 'react-icons/pi'
 
 export default function Login() {
     return (
@@ -11,9 +11,6 @@ export default function Login() {
                 <Button isIconOnly variant='ghost' className='rounded-full' color='primary' startContent={<PiSignInDuotone />}></Button>
             </DropdownTrigger>
             <DropdownMenu>
-                <DropdownItem onPress={() => signIn('github', { redirectTo: '/' })} startContent={<PiGithubLogoDuotone />}>
-                    Log in with GitHub
-                </DropdownItem>
                 <DropdownItem onPress={() => signIn('google', { redirectTo: '/' })} startContent={<PiGoogleLogoDuotone />}>
                     Log in with Google
                 </DropdownItem>
--- auth.ts
+++ auth.ts
@@ -1,10 +1,9 @@
 import NextAuth from 'next-auth'
 import Google from 'next-auth/providers/google'
-import GitHub from 'next-auth/providers/github'
 import { XataAdapter } from '@auth/xata-adapter'
 import { getXataClient } from '@/lib/xata'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: XataAdapter(getXataClient()),
-    providers: [Google, GitHub],
+    providers: [Google],
 })
