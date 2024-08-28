cec473279468b4de7737096eab9038a6eb6b0ef8
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2024-08-28 21:18:08 +0800

    Change AI generation max duration

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -3,7 +3,7 @@ import { getPaper } from '@/components/papers/actions'
 import { isLoggedIn } from '@/utils/auth'
 import { redirect } from 'next/navigation'
 
-export const maxDuration = 120
+export const maxDuration = 60
 
 export async function generateMetadata({ params }: { params: { id: string } }) {
     const paper = await getPaper({ id: params.id })
